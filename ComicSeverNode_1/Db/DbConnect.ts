17f58f758252c7ab962092ca305be3bc54026183
luu khang <luuhoangkhang.93@gmail.com>
2021-12-20 16:07:40 +0700

    aaaa

--- ComicSeverNode_1/Db/DbConnect.ts
+++ ComicSeverNode_1/Db/DbConnect.ts
@@ -1,20 +1,21 @@
 // importing mongoClient to connect at mongodb
-import *as dotenv from 'dotenv'
+require('dotenv').config()
 import { MongoClient, MongoTimeoutError } from 'mongodb';
 import express = require ("express")
 import * as mongo from "mongodb"
 import {User} from   '../entities/UserModel'
-dotenv.config()
+import path = require('path');
+
 // creating a function that execute self runs
 
 export  async function  connect (): Promise<mongo.Db>  {
     try {
-        const connection  = await MongoClient.connect(process.env.BASE_URL)
+        const connection  = await MongoClient.connect(process.env.CONNECTINGSTRING)
         const db = connection.db(process.env.DATA_NAME)
         return db 
         }
     catch(err){
+        console.log(`Connection Error value:${err.toString()}`)
         return err
-        console.log(err.toString())
     }
 }
